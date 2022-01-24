b7be69de9f823aba7e6636727cdd451e2f831d87
lucas552 <lgstehling@gmail.com>
2022-01-24 16:30:54 -0300

    added CORS

--- server.js
+++ server.js
@@ -1,8 +1,13 @@
 const db = require('./db.json')
 const express = require('express')
+const cors = require("cors")
+
 const app = express()
 const port = 3000
 
+app.use(cors(
+  {origin: "*"}
+));
 
 function index(req, res) {
   res.send(JSON.stringify(db))
